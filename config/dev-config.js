d08c3beff0d5674e5498b3a059e854a3fb4388ee
Baron  Shao <garyshore688@gmail.com>
2024-02-01 15:39:28 +0800

    del config

---
+++ config/dev-config.js
@@ -0,0 +1,87 @@
+module.exports = {
+    DEV_CONFIG: {
+        //TOKEN
+        WETH: "",
+        CHI: "",
+        DODO: "",
+
+        //Helper
+        DODOSellHelper: "",
+        DODOCalleeHelper: "",
+        DODOV1PmmHelper: "",
+        DODOV2RouteHelper: "",//0x9B64c81ba54eA51e1f6B7fefb3cfF8AA6F1e2A09
+        CurveSample: "",
+
+
+        //Template
+        CloneFactory: "",
+        FeeRateModel: "",
+        FeeRateImpl: "",
+        UserQuota: "",
+        PermissionManager: "",
+        DVM: "",
+        DPP: "",
+        DSP: "",
+        DPPAdmin: "",
+        CP: "",
+        ERC20MineV3: "",
+
+        ERC20: "",
+        CustomERC20: "",
+        CustomMintableERC20: "",
+
+        //Factory
+        DVMFactory: "",
+        DPPFactory: "",//0xbAb9F4ff4A19a0e8EEBC56b06750253228ffAc6E
+        DSPFactory: "",
+        UpCpFactory: "",
+        CrowdPoolingFactory: "",
+        ERC20Factory: "",
+        ERC20V2Factory: "",
+        ERC20V3Factory: "",
+        DODOMineV3Registry: "",
+
+        //Approve
+        DODOApprove: "",
+        DODOApproveProxy: "",
+
+
+        //Adapter
+        DODOV1Adapter: "",
+        UniAdapter: "",
+        CurveAdapter: "",
+
+
+        //Proxy
+        DODOV2Proxy: "",
+        DSPProxy: "",
+        CpProxy: "",
+        DPPProxy: "",
+        RouteProxy: "",
+        DODOMineV3Proxy: "",
+
+        //Account
+        multiSigAddress: "",
+
+
+        //================== NFT ====================
+        BuyoutModel: "",
+        Fragment: "",
+        NFTCollateralVault: "",//0x95E887aDF9EAa22cC1c6E3Cb7f07adC95b4b25a8
+        DODONFTRouteHelper: "",
+
+        DodoNftErc721: "",
+        DodoNftErc1155: "",
+
+        DODONFTRegistry: "",
+        DODONFTProxy: "",
+
+        //=================== NFTPool ==================
+        DODONFTApprove: "",
+        DODONFTPoolProxy: "",
+        FilterAdmin: "",
+        FilterERC721V1: "",
+        FilterERC1155V1: "",
+        NFTPoolController: ""
+    }
+}
